052ffe6b957fe4e58cfe4a130b96273c891f7638
kevin-gomes <sys20@grupopmz.com.br>
2019-10-16 17:15:30 -0400

    Criação do projeto

---
+++ src/main/java/br/com/kbmg/WsPessoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.kbmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsPessoaApplication.class, args);
+	}
+
+}
