a9878ec36d7c4dd27fc7af7543a2b000b8870712
kevin-gomes <sys20@grupopmz.com.br>
2019-10-17 10:30:27 -0400

    Melhorias na configuração do swagger

--- src/main/java/br/com/kbmg/config/SwaggerConfig.java
+++ src/main/java/br/com/kbmg/config/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package br.com.kbmg.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,12 +18,30 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	@Value("${build.version}")
+	    String version;
+
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 	                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
 	                .paths(PathSelectors.any())
-	                .build();
+	                .build()
+	                .apiInfo(this.apiInfo());
 	}
-
+	 
+    private ApiInfo apiInfo() {
+ 
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title ("API for working with People")
+                .description ("How to manipulate personal data in a micro service.")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .termsOfServiceUrl("/service.html")
+                .version(version)
+                .contact(new Contact("Kevin Gomes","https://www.linkedin.com/in/kevin-gomes-2b5b58175/", "kb.developer.br@gmail.com"))
+                .build();
+ 
+        return apiInfo;
+    }
 }
