ea9c0f7f77565d665d33b6400e1bffece931f0bc
kevin-gomes <sys20@grupopmz.com.br>
2019-10-31 10:30:58 -0400

    Padronização nas rotas do Controller

--- src/main/java/br/com/kbmg/controller/PessoaController.java
+++ src/main/java/br/com/kbmg/controller/PessoaController.java
@@ -4,13 +4,15 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.kbmg.domain.Pessoa;
 import br.com.kbmg.service.PessoaService;
 
-@RestController(value = "/pessoa")
+@RestController
+@RequestMapping(value = "/pessoa")
 public class PessoaController {
 
 	@Autowired
--- src/main/java/br/com/kbmg/controller/PessoaFisicaController.java
+++ src/main/java/br/com/kbmg/controller/PessoaFisicaController.java
@@ -4,19 +4,21 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.kbmg.domain.PessoaFisica;
 import br.com.kbmg.service.PessoaFisicaService;
 
-@RestController(value = "/pessoaFisica")
+@RestController
+@RequestMapping(value = "/pessoaFisica")
 public class PessoaFisicaController {
 
 	@Autowired
 	private PessoaFisicaService service;
 
-	@GetMapping("/findByCpf")
+	@GetMapping(value = "/cpf")
 	public PessoaFisica findByCpf(@Valid @RequestParam String cpf) {
 		return service.findByCpf(cpf);
 	}
