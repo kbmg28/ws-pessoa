987cc17c67c9337ff5cf89642cc3b9177222ea32
kevin-gomes <sys20@grupopmz.com.br>
2019-11-01 14:12:12 -0400

    Busca por id_pf

--- src/main/java/br/com/kbmg/service/impl/PessoaFisicaServiceImpl.java
+++ src/main/java/br/com/kbmg/service/impl/PessoaFisicaServiceImpl.java
@@ -9,18 +9,25 @@ import br.com.kbmg.config.MessagesService;
 import br.com.kbmg.domain.PessoaFisica;
 import br.com.kbmg.repository.PessoaFisicaRepository;
 import br.com.kbmg.service.PessoaFisicaService;
+import br.com.kbmg.utils.Validator;
 
 @Service
 public class PessoaFisicaServiceImpl implements PessoaFisicaService {
 
 	@Autowired
 	PessoaFisicaRepository repository;
-	
+
 	@Autowired
 	MessagesService msg;
-	
+
 	@Override
 	public PessoaFisica findByCpf(String cpf) {
 		return repository.findByCpf(cpf).orElseThrow(() -> new EntityNotFoundException(msg.get("nao.encontrado")));
+	}
+
+	@Override
+	public PessoaFisica findById(String id_pf) {
+		return repository.findById(Validator.stringParseLong(id_pf, "Id da pessoa física"))
+				.orElseThrow(() -> new EntityNotFoundException(msg.get("nao.encontrado")));
 	}
 }
