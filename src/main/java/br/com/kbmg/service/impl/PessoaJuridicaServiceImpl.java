52c409cabfbcba48041c61b578eec2b4a34e16fd
kevin-gomes <sys20@grupopmz.com.br>
2019-11-01 15:04:31 -0400

    Busca por id_pj

--- src/main/java/br/com/kbmg/service/impl/PessoaJuridicaServiceImpl.java
+++ src/main/java/br/com/kbmg/service/impl/PessoaJuridicaServiceImpl.java
@@ -6,24 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.kbmg.config.MessagesService;
-import br.com.kbmg.domain.PessoaFisica;
 import br.com.kbmg.domain.PessoaJuridica;
-import br.com.kbmg.repository.PessoaFisicaRepository;
 import br.com.kbmg.repository.PessoaJuridicaRepository;
-import br.com.kbmg.service.PessoaFisicaService;
 import br.com.kbmg.service.PessoaJuridicaService;
+import br.com.kbmg.utils.Validator;
 
 @Service
 public class PessoaJuridicaServiceImpl implements PessoaJuridicaService {
 
 	@Autowired
 	PessoaJuridicaRepository repository;
-	
+
 	@Autowired
 	MessagesService msg;
-	
+
 	@Override
 	public PessoaJuridica findByCnpj(String cnpj) {
 		return repository.findByCnpj(cnpj).orElseThrow(() -> new EntityNotFoundException(msg.get("nao.encontrado")));
+	}
+
+	@Override
+	public PessoaJuridica findById(String id_pj) {
+		return repository.findById(Validator.stringParseLong(id_pj, "Id da pessoa jurídica"))
+				.orElseThrow(() -> new EntityNotFoundException(msg.get("nao.encontrado")));
 	}
 }
