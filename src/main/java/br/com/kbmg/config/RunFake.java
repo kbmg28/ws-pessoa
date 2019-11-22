decfe6c4eca3804fd3af95c24279d37a3acd9480
Kevin Gomes <kevin28bruno@gmail.com>
2019-11-22 16:15:42 -0400

    Adicionar endereços em RunFake

--- src/main/java/br/com/kbmg/config/RunFake.java
+++ src/main/java/br/com/kbmg/config/RunFake.java
@@ -1,59 +1,130 @@
 package br.com.kbmg.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import br.com.kbmg.domain.Endereco;
 import br.com.kbmg.domain.Pessoa;
 import br.com.kbmg.domain.PessoaFisica;
 import br.com.kbmg.domain.PessoaJuridica;
+import br.com.kbmg.enums.TipoEndereco;
 import br.com.kbmg.enums.TipoPessoa;
 import br.com.kbmg.repository.PessoaRepository;
 
 @Component
-@Profile(value="test")
-public class RunFake implements CommandLineRunner{
+@Profile(value = "test")
+public class RunFake implements CommandLineRunner {
 
 	@Autowired
 	PessoaRepository repository;
-	
+
+	private List<Endereco> listEnd = Arrays.asList(
+			this.novoEndereco("69097150", "Rua Laranjal", null, "Cidade Nova", "56", "Manaus", "AM", 1302603,
+					TipoEndereco.FISCAL),
+			this.novoEndereco("69090080", "Rua Guapiaçu", null, "Cidade Nova", "50", "Manaus", "AM", 1302603,
+					TipoEndereco.FISCAL),
+			this.novoEndereco("69087031", "Rua Charles Darwin", null, "Tancredo Neves", "40", "Manaus", "AM", 1302603,
+					TipoEndereco.OUTROS),
+			this.novoEndereco("69317566", "Rua Sirius", null, "Cidade Satélite", "7", "Boa Vista", "RR", 1400100,
+					TipoEndereco.FISCAL),
+			this.novoEndereco("69317471", "Rua Santa Catarina", null, "Estados", "3A", "Boa Vista", "RR", 1400100,
+					TipoEndereco.FISCAL));
+
+	private Pessoa p;
+
 	@Override
 	public void run(String... args) throws Exception {
-		this.criaPessoaFisica("TESTE DO NOME", "71941528007", "242492757");
-		this.criaPessoaJuridica("NOME DA PESSOA JURIDICA", "19056334000105", "995253323");
-		this.criaPessoaFisica("DEV4DEV GERADOR DE CPF", "47268406061", "363537454");
+		this.criaPessoaFisica("TESTE DO NOME", "71941528007", "242492757", Arrays.asList(0, 1));
+		this.criaPessoaJuridica("NOME DA PESSOA JURIDICA", "19056334000105", "995253323", Arrays.asList(2));
+		this.criaPessoaFisica("DEV4DEV GERADOR DE CPF", "47268406061", "363537454", Arrays.asList(3, 4));
 	}
 
-	private void criaPessoaFisica(String nomeCompleto, String cpf, String rg) {
+	private void criaPessoaFisica(String nomeCompleto, String cpf, String rg, List<Integer> listPosEnd) {
 		PessoaFisica pf = new PessoaFisica();
-		Pessoa p = new Pessoa();
-		
+		p = new Pessoa();
+
 		p.setNomeCompleto(nomeCompleto);
 		p.setTipoPessoa(TipoPessoa.PF);
 		p.setPessoaFisica(pf);
-		
+
 		pf.setCpf(cpf);
 		pf.setRg(rg);
 		pf.setPessoa(p);
-		
+
+		p = repository.save(p);
+
+		listPosEnd.forEach(pos -> this.criaEndereco(p, pos));
+
 		repository.save(p);
 	}
 
-	private void criaPessoaJuridica(String nomeCompleto, String cnpj, String inscricaoEstadual) {
+	private void criaPessoaJuridica(String nomeCompleto, String cnpj, String inscricaoEstadual,
+			List<Integer> listPosEnd) {
 		PessoaJuridica pj = new PessoaJuridica();
-		Pessoa p = new Pessoa();
-		
+		p = new Pessoa();
+
 		p.setNomeCompleto(nomeCompleto);
 		p.setTipoPessoa(TipoPessoa.PJ);
 		p.setPessoaJuridica(pj);
-		
+
 		pj.setCnpj(cnpj);
 		pj.setInscricaoEstadual(inscricaoEstadual);
 		pj.setPessoa(p);
-		
+
+		p = repository.save(p);
+
+		listPosEnd.forEach(pos -> this.criaEndereco(p, pos));
+
 		repository.save(p);
 	}
-	
-	
+
+	private void criaEndereco(Pessoa p, Integer pos) {
+		Endereco endereco = this.getEndereco(listEnd.get(pos));
+
+		endereco.setIdEndereco(pos.longValue() + 1);
+		endereco.setPessoa(p);
+		p.getEnderecos().add(endereco);
+	}
+
+	private Endereco getEndereco(Endereco end) {
+		return new Endereco() {
+			private static final long serialVersionUID = 1L;
+			{
+				setCep(end.getCep());
+				setLogradouro(end.getLogradouro());
+				setComplemento(end.getComplemento());
+				setBairro(end.getBairro());
+				setNumero(end.getNumero());
+				setLocalidade(end.getLocalidade());
+				setUf(end.getUf());
+				setIbge(end.getIbge());
+				setTipoEndereco(end.getTipoEndereco());
+			}
+		};
+	}
+
+	private Endereco novoEndereco(String cep, String logradouro, String complemento, String bairro, String numero,
+			String localidade, String uf, Integer ibge, TipoEndereco tipoEndereco) {
+		return new Endereco() {
+			private static final long serialVersionUID = 1L;
+			{
+				setCep(cep);
+				setLogradouro(logradouro);
+				setComplemento(complemento);
+				setBairro(bairro);
+				setNumero(numero);
+				setLocalidade(localidade);
+				setUf(uf);
+				setIbge(ibge);
+				setTipoEndereco(tipoEndereco);
+
+			}
+		};
+	}
+
 }
