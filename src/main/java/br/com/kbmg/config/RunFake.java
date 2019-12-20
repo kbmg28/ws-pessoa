2f8686375be6ee311d914cf5aefff8e132894031
Kevin Gomes <kevin28bruno@gmail.com>
2019-12-20 16:24:27 -0400

    Adicionando Email em RunFake

--- src/main/java/br/com/kbmg/config/RunFake.java
+++ src/main/java/br/com/kbmg/config/RunFake.java
@@ -8,10 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import br.com.kbmg.domain.Email;
 import br.com.kbmg.domain.Endereco;
 import br.com.kbmg.domain.Pessoa;
 import br.com.kbmg.domain.PessoaFisica;
 import br.com.kbmg.domain.PessoaJuridica;
+import br.com.kbmg.enums.TipoDeUso;
 import br.com.kbmg.enums.TipoEndereco;
 import br.com.kbmg.enums.TipoPessoa;
 import br.com.kbmg.repository.PessoaRepository;
@@ -34,6 +36,12 @@ public class RunFake implements CommandLineRunner {
 					TipoEndereco.FISCAL),
 			this.novoEndereco("69317471", "Rua Santa Catarina", null, "Estados", "3A", "Boa Vista", "RR", 1400100,
 					TipoEndereco.FISCAL));
+
+	private List<Email> listEmail = Arrays.asList(this.novoEmail("teste@teste.com", TipoDeUso.PARTICULAR),
+			this.novoEmail("teste2@teste.com", TipoDeUso.OUTROS),
+			this.novoEmail("teste3@teste.com", TipoDeUso.PARTICULAR),
+			this.novoEmail("teste4@teste.com", TipoDeUso.PARTICULAR),
+			this.novoEmail("teste5@teste.com", TipoDeUso.CORPORATIVO));
 
 	private Pessoa p;
 
@@ -58,7 +66,10 @@ public class RunFake implements CommandLineRunner {
 
 		p = repository.save(p);
 
-		listPosEnd.forEach(pos -> this.criaEndereco(p, pos));
+		listPosEnd.forEach(pos -> {
+			this.criaEndereco(p, pos);
+			this.criaEmail(p, pos);
+		});	
 
 		repository.save(p);
 	}
@@ -78,9 +89,31 @@ public class RunFake implements CommandLineRunner {
 
 		p = repository.save(p);
 
-		listPosEnd.forEach(pos -> this.criaEndereco(p, pos));
+		listPosEnd.forEach(pos -> {
+			this.criaEndereco(p, pos);
+			this.criaEmail(p, 0);
+		});
 
 		repository.save(p);
+	}
+
+	private void criaEmail(Pessoa p, Integer pos) {
+		Email email = this.getEmail(listEmail.get(pos));
+		
+		email.setIdEmail(pos.longValue() + 1);
+		email.setPessoa(p);
+		
+		p.getEmails().add(email);
+	}
+
+	private Email getEmail(Email email) {
+		return new Email() {
+			private static final long serialVersionUID = 1L;
+			{
+				setEmail(email.getEmail());
+				setTipoDeUso(email.getTipoDeUso());
+			}
+		};
 	}
 
 	private void criaEndereco(Pessoa p, Integer pos) {
@@ -123,6 +156,16 @@ public class RunFake implements CommandLineRunner {
 				setIbge(ibge);
 				setTipoEndereco(tipoEndereco);
 
+			}
+		};
+	}
+
+	private Email novoEmail(String email, TipoDeUso tipoDeUso) {
+		return new Email() {
+			private static final long serialVersionUID = 1L;
+			{
+				setEmail(email);
+				setTipoDeUso(tipoDeUso);
 			}
 		};
 	}
