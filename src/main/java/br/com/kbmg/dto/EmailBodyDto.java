3b85c74226af838038a598eeae6f1260e4d8c7cf
Kevin Gomes <kevin28bruno@gmail.com>
2020-01-08 17:27:48 -0400

    EmailBodyDto

---
+++ src/main/java/br/com/kbmg/dto/EmailBodyDto.java
@@ -0,0 +1,42 @@
+package br.com.kbmg.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+import br.com.kbmg.enums.TipoDeUsoEnum;
+
+public class EmailBodyDto {
+
+	private String idEmail;
+	
+	@Email(message="email inválido.")
+	private String email;
+	
+	@NotNull(message="tipoDeUso não informado.")
+	private TipoDeUsoEnum tipoDeUso;
+	
+	public String getIdEmail() {
+		return idEmail;
+	}
+
+	public void setIdEmail(String idEmail) {
+		this.idEmail = idEmail;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public TipoDeUsoEnum getTipoDeUso() {
+		return tipoDeUso;
+	}
+
+	public void setTipoDeUso(TipoDeUsoEnum tipoDeUso) {
+		this.tipoDeUso = tipoDeUso;
+	}
+
+}
